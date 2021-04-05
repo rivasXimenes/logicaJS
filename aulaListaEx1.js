5d8835ddecc35277cda7ce39745226ffc96a00f4
rivasXimenes <rivaldoaraujo971@gmail.com>
2021-04-05 15:10:17 -0300

    dia1

--- aulaListaEx1.js
+++ aulaListaEx1.js
@@ -1,8 +1,8 @@
 /* 
-Supondo que A, B e C são variáveis do tipo inteiro, 
-com valores iguais a 5, 10 e -8, respectivamente e uma 
-variável D com valor 1.5, quais os resultados das expressões 
-aritméticas apresentadas em seguida? 
+Supondo que A, B e C são variáveis do tipo inteiro,  
+com valores iguais a 5, 10 e -8, respectivamente e
+uma variável D com valor 1.5, quais os resultados das 
+expressões aritméticas apresentadas em seguida? 
 */
 
 let a = 5 
